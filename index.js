617836b97281117185284fc285ff52bb308aa015
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-03 12:11:18 +0530

    new commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 let quote = document.getElementById("quote")
 let author = document.getElementById("author")
+let saved = document.getElementById("saved")
+
+// const save = () => {
+//     localStorage.setItem("savedQuote", realData[a].author);
+//     document.getElementById("saved").innerHTML = localStorage.getItem("savedQuote");
+// }
 
 const getQuote = async () => {
     const quotes = 'https://type.fit/api/quotes';
     try {
         const data = await fetch(quotes)
         const realData = await data.json();
-        // console.log(data.length())
-        // console.log(realData.length)
-        // console.log(a);
 
-        // let a = Math.floor(Math.random()*realData.length)
-        let a = Math.floor(Math.random()*20)
+        let a = Math.floor(Math.random()*realData.length)
         console.log(realData[a].text)
-        quote.innerHTML=realData[a].text
+        quote.innerHTML = realData[a].text
 
-        realData[a].author===null?realData[a].author="Anonymous":realData[a].author
+        realData[a].author === null ? realData[a].author = "Anonymous" : realData[a].author
         console.log(realData[a].author)
-        author.innerHTML=realData[a].author
+        author.innerHTML = realData[a].author
     } catch (error) {
 
     }
